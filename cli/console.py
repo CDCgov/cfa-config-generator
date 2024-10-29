47932097ef6112bd665bb2eab550467256c1e723
amondal2 <ab59@cdc.gov>
2024-10-29 19:44:24 +0000

    implement inspect_task

--- cli/console.py
+++ cli/console.py
@@ -1,7 +1,8 @@
+import json
+
 import typer
-from azure.core.exceptions import ClientAuthenticationError
+from azure.core.exceptions import ClientAuthenticationError, ResourceNotFoundError
 from azure.identity import CredentialUnavailableError
-from rich import print
 from rich.console import Console
 from typing_extensions import Annotated
 
@@ -25,7 +26,7 @@ def check_login():
             sp_credential.get_token(azure_storage["scope_url"])
         except (CredentialUnavailableError, ClientAuthenticationError) as e:
             console.print(
-                "[italic red] You are not logged into Azure. Run `az login` to log in."
+                "[italic red] :triangular_flag: You are not logged into Azure. Run `az login` to log in."
             )
             raise e
 
@@ -36,7 +37,8 @@ def check_login():
 def list_jobs():
     container_name = azure_storage["azure_container_name"]
     with console.status(
-        f"Fetching jobs in {container_name} container...\n", spinner="aesthetic"
+        f"Fetching jobs in {container_name} container...\n",
+        spinner="aesthetic",
     ):
         sp_credential = obtain_sp_credential()
         try:
@@ -48,9 +50,9 @@ def list_jobs():
             blob_list = container_client.list_blobs()
             unique_jobs = get_unique_jobs_from_blobs(blob_list=blob_list)
             console.print(unique_jobs)
-        except ValueError as e:
+        except (ValueError, ResourceNotFoundError, ClientAuthenticationError) as e:
             console.print(
-                "[italic red] Error instantiating blob client. Check your credentials."
+                "[italic red] :triangular_flag: Error instantiating blob client or finding specified resource."
             )
             raise e
 
@@ -76,16 +78,44 @@ def list_tasks(
             blob_list = container_client.list_blobs()
             tasks_for_job = get_tasks_for_job_id(blob_list=blob_list, job_id=job_id)
             console.print(tasks_for_job)
-        except ValueError as e:
+        except (ResourceNotFoundError, ValueError, ClientAuthenticationError) as e:
             console.print(
-                "[italic red] Error instantiating blob client. Check your credentials."
+                "[italic red] :triangular_flag: Error instantiating blob client or finding specified resource."
             )
             raise e
 
 
 @app.command("inspect-task")
-def inspect_task(job_id: str, task_id: str):
-    print(f"Inspecting task {task_id} for job {job_id}: ")
+def inspect_task(
+    job_id: Annotated[
+        str, typer.Option("--job-id", "-j", help="Job ID to list tasks for")
+    ],
+    task_filename: Annotated[
+        str, typer.Option("--task-filename", "-t", help="Task filename to inspect")
+    ],
+):
+    with console.status(
+        f"Inspecting task in {task_filename} container for job_id {job_id}...\n",
+        spinner="aesthetic",
+    ):
+        sp_credential = obtain_sp_credential()
+        full_blob_path = f"{job_id}/{task_filename}"
+        try:
+            blob_client = instantiate_blob_client(
+                sp_credential=sp_credential,
+                account_url=azure_storage["azure_storage_account_url"],
+            )
+            blob_client = blob_client.get_blob_client(
+                container=azure_storage["azure_container_name"], blob=full_blob_path
+            )
+            downloader = blob_client.download_blob(max_concurrency=1, encoding="utf-8")
+            blob_text = downloader.readall()
+            console.print(json.loads(blob_text))
+        except (ResourceNotFoundError, ValueError, ClientAuthenticationError) as e:
+            console.print(
+                "[italic red] :triangular_flag: Error instantiating blob client or finding specified resource."
+            )
+            raise e
 
 
 def main():
