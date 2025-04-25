fc5206ab9a87cda9dcad637e2ca10cb959b7a04a
micahwiesner67 <micahwiesner67@gmail.com>
2025-04-25 20:49:40 +0000

    adding cmdstanr as backend in shared params

--- src/cfa_config_generator/utils/epinow2/constants.py
+++ src/cfa_config_generator/utils/epinow2/constants.py
@@ -3,6 +3,7 @@ shared_params = {
     "horizon": 14,
     "priors": {"rt": {"mean": 1.0, "sd": 0.2}, "gp": {"alpha_sd": 0.01}},
     "sampler_opts": {
+        "backend": "cmdstanr",
         "cores": 4,
         "chains": 4,
         "iter_warmup": 5000,
