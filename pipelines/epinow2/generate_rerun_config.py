fa876fa0e27b6a6ad69f83751dc9f2eeca1256e6
Nate McIntosh <NMcIntosh@cdc.gov>
2025-04-25 15:52:28 -0400

    forgot to make rerun args explicit

--- pipelines/epinow2/generate_rerun_config.py
+++ pipelines/epinow2/generate_rerun_config.py
@@ -21,4 +21,16 @@ if __name__ == "__main__":
     data_exclusions_path: str | None = os.getenv("data_exclusions_path")
 
     # generate_rerun_config() now accepts arguments directly
-    generate_rerun_config(data_exclusions_path=data_exclusions_path, **user_args)
+    generate_rerun_config(
+        state=user_args["state"],
+        disease=user_args["disease"],
+        report_date=user_args["report_date"],
+        reference_dates=user_args["reference_dates"],
+        data_path=user_args["data_path"],
+        data_container=user_args["data_container"],
+        production_date=user_args["production_date"],
+        job_id=user_args["job_id"],
+        as_of_date=user_args["as_of_date"],
+        output_container=user_args["output_container"],
+        data_exclusions_path=data_exclusions_path,
+    )
