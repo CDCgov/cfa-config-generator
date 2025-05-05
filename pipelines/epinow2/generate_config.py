5c0b43d2f4464eb441aab1cdd2b18f9b6584d744
Nate McIntosh <natemcintosh@gmail.com>
2025-05-05 11:09:15 -0400

    print env vars as they first appear

--- pipelines/epinow2/generate_config.py
+++ pipelines/epinow2/generate_config.py
@@ -1,11 +1,7 @@
 import logging
-from pprint import pprint
-from typing import Any
+import os
 
-from cfa_config_generator.utils.epinow2.functions import (
-    extract_user_args,
-    generate_timestamp,
-)
+from cfa_config_generator.utils.epinow2.functions import generate_timestamp
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +9,32 @@ if __name__ == "__main__":
     # Generate job-specific parameters
     as_of_date: str = generate_timestamp()
 
-    # Pull run parameters from environment
-    user_args: dict[str, Any] = extract_user_args(as_of_date=as_of_date)
+    task_exclusions = os.getenv("task_exclusions")
+    exclusions = os.getenv("exclusions")
+    state = os.getenv("state")
+    disease = os.getenv("disease")
 
-    # Print out all the user_args, so we can see what the defaults are
-    pprint(user_args)
+    # Handle report_date
+    report_date_str = os.getenv("report_date")
+
+    # Handle production_date
+    production_date_str = os.getenv("production_date")
+
+    reference_dates_str = os.getenv("reference_dates")
+
+    data_container = os.getenv("data_container")
+    output_container = os.getenv("output_container")
+    job_id = os.getenv("job_id")
+
+    # Print out all of the above variables
+    print("Environment Variables:")
+    print(f"{task_exclusions=}")
+    print(f"{exclusions=}")
+    print(f"{state=}")
+    print(f"{disease=}")
+    print(f"{report_date_str=}")
+    print(f"{production_date_str=}")
+    print(f"{reference_dates_str=}")
+    print(f"{data_container=}")
+    print(f"{output_container=}")
+    print(f"{job_id=}")
