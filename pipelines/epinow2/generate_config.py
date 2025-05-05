61212cac5ac028ed7fc92c0aaa7593ef2797d80f
Nate McIntosh <natemcintosh@gmail.com>
2025-05-05 10:54:36 -0400

    for seeing what GHA passes in as defaults
    specifically, when the user doesn't specify. Does it do
    `null`, "", ...?

--- pipelines/epinow2/generate_config.py
+++ pipelines/epinow2/generate_config.py
@@ -1,7 +1,7 @@
 import logging
+from pprint import pprint
 from typing import Any
 
-from cfa_config_generator.utils.epinow2.driver_functions import generate_config
 from cfa_config_generator.utils.epinow2.functions import (
     extract_user_args,
     generate_timestamp,
@@ -16,17 +16,5 @@ if __name__ == "__main__":
     # Pull run parameters from environment
     user_args: dict[str, Any] = extract_user_args(as_of_date=as_of_date)
 
-    generate_config(
-        state=user_args["state"],
-        disease=user_args["disease"],
-        report_date=user_args["report_date"],
-        reference_dates=user_args["reference_dates"],
-        data_path=user_args["data_path"],
-        data_container=user_args["data_container"],
-        production_date=user_args["production_date"],
-        job_id=user_args["job_id"],
-        as_of_date=user_args["as_of_date"],
-        output_container=user_args["output_container"],
-        task_exclusions=user_args.get("task_exclusions"),
-        exclusions=user_args.get("exclusions"),
-    )
+    # Print out all the user_args, so we can see what the defaults are
+    pprint(user_args)
--- pipelines/epinow2/generate_rerun_config.py
+++ pipelines/epinow2/generate_rerun_config.py
@@ -1,8 +1,8 @@
 import logging
 import os
+from pprint import pprint
 from typing import Any
 
-from cfa_config_generator.utils.epinow2.driver_functions import generate_rerun_config
 from cfa_config_generator.utils.epinow2.functions import (
     extract_user_args,
     generate_timestamp,
@@ -20,17 +20,7 @@ if __name__ == "__main__":
     # Get the data_exclusions_path separately from the environment
     data_exclusions_path: str | None = os.getenv("data_exclusions_path")
 
-    # generate_rerun_config() now accepts arguments directly
-    generate_rerun_config(
-        state=user_args["state"],
-        disease=user_args["disease"],
-        report_date=user_args["report_date"],
-        reference_dates=user_args["reference_dates"],
-        data_path=user_args["data_path"],
-        data_container=user_args["data_container"],
-        production_date=user_args["production_date"],
-        job_id=user_args["job_id"],
-        as_of_date=user_args["as_of_date"],
-        output_container=user_args["output_container"],
-        data_exclusions_path=data_exclusions_path,
-    )
+    # Print out all the user args, and the data_exclusions_path so we can see the
+    # defaults
+    pprint(user_args)
+    pprint(data_exclusions_path)
